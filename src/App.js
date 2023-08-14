5bfef97e19f89b2c783710e2f6c7c6d0097e1620
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-08-14 13:01:47 +0100

    added navbar + routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
 function App() {
   return (
     <div className="App">
         <h1>App</h1>
+        <nav>
+          <ul id="navigation">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route exact path='/' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route exact path='/contact' element={<Contact/>}/>
+        </Routes>
+
+
     </div>
   );
 }
