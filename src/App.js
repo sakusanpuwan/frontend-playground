efc631fe9f69e8464b75eba4e465a9c59e5a9d26
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-08-14 15:56:04 +0100

    added navbar with styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import Image from './components/Image';
+import Text from './components/Text';
+import Other from './components/Other';
 
 function App() {
   return (
     <div className="App">
-        <h1>App</h1>
-        <nav>
-          <ul id="navigation">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-              <li><Link to="/contact">Contact</Link></li>
+        <h1>Frontend Playground</h1>
+        <nav className='navbar'>
+          <ul className="navigation">
+              <li><Link to="/">Image</Link></li>
+              <li><Link to="/text">Text</Link></li>
+              <li><Link to="/other">Other</Link></li>
           </ul>
         </nav>
 
         <Routes>
-          <Route exact path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route exact path='/contact' element={<Contact/>}/>
+          <Route exact path='/' element={<Image/>}/>
+          <Route path='/text' element={<Text/>}/>
+          <Route exact path='/other' element={<Other/>}/>
         </Routes>
+
 
 
     </div>
