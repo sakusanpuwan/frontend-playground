3152e700ccaa260ed4284c89458ad6d065f89d63
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-08-14 15:56:21 +0100

    created page components

---
+++ src/components/Image.js
@@ -0,0 +1,10 @@
+const Image = () => {
+    return(
+        <div>
+            <h1>Image</h1>
+
+        </div>
+    )
+}
+
+export default Image;
---
+++ src/components/Other.js
@@ -0,0 +1,9 @@
+const Other = () => {
+    return(
+        <div>
+            <h1>Other</h1>
+        </div>
+    )
+}
+
+export default Other;
