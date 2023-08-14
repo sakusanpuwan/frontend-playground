3236e5bd529566b408fbc5ecc63ef97ef07f12e8
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-08-14 12:34:38 +0100

    check if CI/CD works

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Test to check if CI/CD works
         </p>
         <a
           className="App-link"
