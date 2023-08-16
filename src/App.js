38473238f1cf31b35a07111c63a6b1886b79b7c0
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-08-16 17:29:54 +0100

    added google fonts

--- src/App.js
+++ src/App.js
@@ -7,20 +7,20 @@ import Other from './components/Other';
 function App() {
   return (
     <div className="App">
-        <h1>Frontend Playground</h1>
-        <nav className='navbar'>
-          <ul className="navigation">
-              <li><Link to="/">Image</Link></li>
-              <li><Link to="/text">Text</Link></li>
-              <li><Link to="/other">Other</Link></li>
-          </ul>
-        </nav>
+      <h1 style={{fontFamily: "Tilt Prism"}}>Frontend Playground 🎨</h1>
+      <nav className='navbar'>
+        <ul className="navigation">
+            <li><Link to="/">Image</Link></li>
+            <li><Link to="/text">Text</Link></li>
+            <li><Link to="/other">Other</Link></li>
+        </ul>
+      </nav>
 
-        <Routes>
-          <Route exact path='/' element={<Image/>}/>
-          <Route path='/text' element={<Text/>}/>
-          <Route exact path='/other' element={<Other/>}/>
-        </Routes>
+      <Routes>
+        <Route exact path='/' element={<Image/>}/>
+        <Route path='/text' element={<Text/>}/>
+        <Route exact path='/other' element={<Other/>}/>
+      </Routes>
 
 
 
